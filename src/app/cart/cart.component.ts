2871088ce5751611cea24eea797df1aa29d8ca40
Arivazhagan Selvaraj <arivazhagan.selvaraj@aziro.com>
2025-11-24 16:26:01 +0530

    cart page phone list rendered, intoduced bootstrap icons

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, numberAttribute } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { CartListComponent } from './cart-list/cart-list.component';
 import { CartSummaryComponent } from './cart-summary/cart-summary.component';
+import { selectCartCount } from '../store/cart/cart.selectors';
 
 @Component({
   selector: 'app-cart',
@@ -12,7 +15,16 @@ import { CartSummaryComponent } from './cart-summary/cart-summary.component';
   styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent {
-  constructor(private router: Router) {}
+  cartCount$!: Observable<number>;
+  cartCount: number = 0;
+
+  constructor(private router: Router, private store: Store) {
+    this.cartCount$ = this.store.select(selectCartCount);
+
+    this.cartCount$.subscribe(count => {
+      this.cartCount = count;
+    });
+  }
 
   onProceedToCheckout() {
     this.router.navigate(['/checkout']);
